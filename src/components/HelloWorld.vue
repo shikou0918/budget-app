554fba8aa074ba325ccb4a4daee92406bdb15e68
shikou0918 <shikou.0918@gmail.com>
2024-08-15 08:04:51 +0900

    [update] Pie Chart追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,16 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js/auto';
+import { Pie } from 'vue-chartjs'
+import * as chartConfig from '../chartConfig.js'
 
-defineProps<{ msg: string }>()
+// ChartJSのプラグインを登録
+ChartJS.register(ArcElement, Tooltip, Legend)
 
-const count = ref(0)
+// chartConfigからデータを取得
+const { data, options } = chartConfig
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <Pie :data="data" :options="options" />
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
